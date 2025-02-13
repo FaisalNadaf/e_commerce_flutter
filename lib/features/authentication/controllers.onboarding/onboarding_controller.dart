322b7bf1a803c4e5a2fbeb18ce677dba12bcbdbb
Faisal <faisalfnadaf@gmail.com>
2025-02-13 21:28:42 +0530

    Implement onboarding flow with new App structure and controller

---
+++ lib/features/authentication/controllers.onboarding/onboarding_controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class OnboardingController extends GetxController {
+  static OnboardingController get to => Get.find();
+
+  void updatePageIndicator(index) {}
+  void ddatNavigationClick(index) {}
+  void nextPage() {}
+
+  void skipPage() {}
+}
---
+++ lib/features/authentication/screens/onboarding.dart
@@ -0,0 +1,75 @@
+import 'package:e_commerce/utils/constants/image_strings.dart';
+import 'package:e_commerce/utils/constants/sizes.dart';
+import 'package:e_commerce/utils/constants/text_strings.dart';
+import 'package:e_commerce/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatelessWidget {
+  const Onboarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            children: [
+              onBoardingWidget(
+                image: TImages.onBoardingImage1,
+                title: TTexts.onBoardingTitle1,
+                subTitle: TTexts.onBoardingSubTitle1,
+              ),
+              onBoardingWidget(
+                image: TImages.onBoardingImage2,
+                title: TTexts.onBoardingTitle2,
+                subTitle: TTexts.onBoardingSubTitle2,
+              ),
+              onBoardingWidget(
+                image: TImages.onBoardingImage3,
+                title: TTexts.onBoardingTitle3,
+                subTitle: TTexts.onBoardingSubTitle3,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class onBoardingWidget extends StatelessWidget {
+  const onBoardingWidget({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subTitle,
+  });
+
+  final String image, title, subTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(TSizes.defaultSpace),
+      child: Column(
+        children: [
+          Image(
+            height: THelperFunctions.screenHeight() * 0.6,
+            width: THelperFunctions.screenWidth() * 0.8,
+            image: AssetImage(image),
+          ),
+          Text(
+            title,
+            style: Theme.of(context).textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            subTitle,
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
