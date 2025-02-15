6e704fba3dc321cc38961ab111417388234bceb8
Faisal <faisalfnadaf@gmail.com>
2025-02-15 23:53:00 +0530

    Refactor onboarding screen by adding dot navigation and next button components

--- lib/features/authentication/screens/onboarding_page.dart/onboarding.dart
+++ lib/features/authentication/screens/onboarding_page.dart/onboarding.dart
@@ -1,13 +1,10 @@
+import 'package:e_commerce/features/authentication/screens/widgets/OnBoardingDotNavigation.dart';
+import 'package:e_commerce/features/authentication/screens/widgets/OnBoardingNextButton.dart';
 import 'package:e_commerce/features/authentication/screens/widgets/onBoardingSkipButton.dart';
 import 'package:e_commerce/features/authentication/screens/widgets/onBoardingWidget.dart';
-import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:e_commerce/utils/constants/image_strings.dart';
-import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:e_commerce/utils/constants/text_strings.dart';
-import 'package:e_commerce/utils/device/device_utility.dart';
-import 'package:e_commerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class Onboarding extends StatelessWidget {
   const Onboarding({super.key});
@@ -18,7 +15,7 @@ class Onboarding extends StatelessWidget {
       body: Stack(
         children: [
           PageView(
-            children: [
+            children: const [
               onBoardingWidget(
                 image: TImages.onBoardingImage1,
                 title: TTexts.onBoardingTitle1,
@@ -39,18 +36,9 @@ class Onboarding extends StatelessWidget {
 // skip button
           onBoardingSkipButton(),
 
-          Positioned(
-            bottom: TDeviceUtils.getBottomNavigationBarHeight() + 25,
-            left: TSizes.defaultSpace,
-            child: SmoothPageIndicator(
-              controller: PageController(),
-              count: 3,
-              effect: ExpandingDotsEffect(
-                dotColor: TColors.dark,
-                dotHeight: 6,
-              ),
-            ),
-          ),
+          const OnBoardingDotNavigation(),
+
+          const OnBoardingNextButton(),
         ],
       ),
     );
---
+++ lib/features/authentication/screens/widgets/OnBoardingNextButton.dart
@@ -0,0 +1,30 @@
+import 'package:e_commerce/utils/constants/colors.dart';
+import 'package:e_commerce/utils/device/device_utility.dart';
+import 'package:e_commerce/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../utils/constants/sizes.dart';
+
+class OnBoardingNextButton extends StatelessWidget {
+  const OnBoardingNextButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Positioned(
+      bottom: TDeviceUtils.getBottomNavigationBarHeight() + 25,
+      right: TSizes.defaultSpace,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: dark ? TColors.primary : TColors.dark,
+          shape: const CircleBorder(),
+        ),
+        onPressed: () {},
+        child: const Icon(Icons.arrow_forward),
+      ),
+    );
+  }
+}
