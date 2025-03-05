b02ff2bb3b63acd7f3ba5cfa80fad556eb4b19da
Faisal <faisalfnadaf@gmail.com>
2025-03-05 23:57:55 +0530

    Add tap functionality to search container; implement vertical image text widget and home categories section

--- lib/common/widgets/custom_shapes/containers/search_container.dart
+++ lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -13,40 +13,45 @@ class TSearchContainer extends StatelessWidget {
     this.showBackground = true,
     this.showBorder = true,
     this.icon = Iconsax.search_normal,
+    this.onTap,
   });
 
   final String text;
   final bool showBackground, showBorder;
   final IconData? icon;
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
-      child: Container(
-        width: TDeviceUtils.getScreenWidth(context),
-        padding: const EdgeInsets.all(TSizes.md),
-        decoration: BoxDecoration(
-          color: showBackground
-              ? dark
-                  ? TColors.dark
-                  : TColors.light
-              : Colors.transparent,
-          borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
-          border: showBorder
-              ? Border.all(color: dark ? TColors.dark : TColors.light)
-              : null,
-        ),
-        child: Row(
-          children: [
-            Icon(
-              icon,
-              color: dark ? TColors.light : TColors.darkerGrey,
-            ),
-            const SizedBox(width: TSizes.spaceBtwItems),
-            Text(text, style: Theme.of(context).textTheme.bodySmall),
-          ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace),
+        child: Container(
+          width: TDeviceUtils.getScreenWidth(context),
+          padding: const EdgeInsets.all(TSizes.md),
+          decoration: BoxDecoration(
+            color: showBackground
+                ? dark
+                    ? TColors.dark
+                    : TColors.light
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
+            border: showBorder
+                ? Border.all(color: dark ? TColors.dark : TColors.light)
+                : null,
+          ),
+          child: Row(
+            children: [
+              Icon(
+                icon,
+                color: dark ? TColors.light : TColors.darkerGrey,
+              ),
+              const SizedBox(width: TSizes.spaceBtwItems),
+              Text(text, style: Theme.of(context).textTheme.bodySmall),
+            ],
+          ),
         ),
       ),
     );
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,16 +1,21 @@
-import 'package:e_commerce/common/widgets/texts/section_heading.dart';
+import 'package:e_commerce/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:e_commerce/utils/constants/image_strings.dart';
+import 'package:e_commerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
 import 'package:e_commerce/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:e_commerce/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:e_commerce/common/widgets/texts/section_heading.dart';
 import 'package:e_commerce/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 
 class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -19,41 +24,92 @@ class HomeScreen extends StatelessWidget {
             TPrimaryHeaderContainer(
               child: Column(
                 children: [
-                  HomeAppBar(),
-
-                  SizedBox(height: TSizes.spaceBtwSections),
-
+                  // App bar
+                  const HomeAppBar(),
+                  const SizedBox(height: TSizes.spaceBtwSections),
                   // Search bar
-                  TSearchContainer(text: "Search in Store"),
-
-                  SizedBox(height: TSizes.spaceBtwSections),
-
+                  const TSearchContainer(text: "Search in Store"),
+                  const SizedBox(height: TSizes.spaceBtwSections),
                   // Categories
                   Padding(
-                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    padding: const EdgeInsets.only(left: TSizes.defaultSpace),
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         TSectionHeading(
                             title: "Product Categories",
+                            textColor: TColors.white,
                             showActionButton: false),
                         const SizedBox(height: TSizes.spaceBtwItems),
-
-
-
-
-ListView.builder(itemBuilder: (context, index) {
-
-
+                        const THomeCategories()
                       ],
                     ),
                   ),
                 ],
               ),
             ),
+
+            // Body
+
+            Padding(
+              padding: const EdgeInsets.all(TSizes.defaultSpace),
+              child: TRoundedImage(dark: dark),
+            )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class TRoundedImage extends StatelessWidget {
+  const TRoundedImage({
+    super.key,
+    required this.applyImageRadius,
+    required this.isNetworkImage,
+    required this.borderRadius,
+    this.height,
+    this.width,
+    this.imageUrl,
+    this.border,
+    this.backgroundColor,
+    this.fit,
+    this.padding,
+    this.onPressed,
+  });
+
+  final bool applyImageRadius, isNetworkImage;
+  final double? height, width;
+  final String? imageUrl;
+  final BoxBorder? border;
+  final Color? backgroundColor;
+  final BoxFit? fit;
+  final EdgeInsetsGeometry? padding;
+  final double borderRadius;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        height: height,
+        width: width,
+        padding: padding,
+        decoration: BoxDecoration(
+          border: border,
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+        child: ClipRRect(
+          borderRadius: applyImageRadius
+              ? BorderRadius.circular(borderRadius)
+              : BorderRadius.zero,
+          child: Image(
+            image: AssetImage(TImages.promoBanner1),
+            fit: BoxFit.contain,
+          ),
         ),
       ),
     );
